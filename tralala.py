1655e5a5ae80bff93e03b73ca1af9ad4d76168dd
Nyomancer <nyomancer@gmail.com>
2013-09-28 09:48:56 +0200

    added ctcp version info, namesand comments, cleaned up code ab bit...

--- tralala.py
+++ tralala.py
@@ -5,26 +5,42 @@ from twisted.internet import protocol
 
 
 class TralalaBot(irc.IRCClient):
+    
+    #CTCP VERSION request details
+    versionName = "TralalaBot"
+    versionNum = "0.1.0"
+    
+    #gets nick from factory
     def _get_nickname(self):
         return self.factory.nickname
     
+    #set _ALL_ the names
     nickname = property(_get_nickname)
-    
+    realname = nickname
+    username = nickname
+
+    #called after sucessfully signing on to the server.
     def signedOn(self):
         self.join(self.factory.channel)
+        #TODO: pipe this to logger-object instead of stdout
+        #also TODO: build a gorram logger-class
         print "Signed on as %s." % (self.nickname,)
 
+    #called when I finish joining a channel
     def joined(self, channel):
+        #TODO: pipe this to logger-object instead of stdout
         print "Joined %s." % (channel,)
 
+    #called when I have a message from a user to me or a channel
     def privmsg(self, user, channel, msg):
-        print "%s: %s" % (user, msg,)
-        if msg == "teststring123":
-             self.msg(channel, "teststring456")
-        elif (user.split('!')[0] == "chke") and (channel == self.nickname) and (msg == "opmefaggot"):
+
+        #TODO: build a backlog feature
+
+        #grants channelop to owner after sending the right "password" via query
+        if (user.split('!')[0] == "chke") and (channel == self.nickname) and (msg == "opmefaggot"):
+            #TODO: pipe this to logger-object instead of stdout
             print "%s %s requested OP: granted" % ((user.split('!')[0]), channel,)
             self.mode(self.factory.channel, True, "o", None, "chke")
-           # self.msg((user.split('!')[0]), "testanswer")
 
 
 class TralalaBotFactory(protocol.ClientFactory):
@@ -36,20 +52,21 @@ class TralalaBotFactory(protocol.ClientFactory):
 
 
     def clientConnectionLost(self, connector, reason):
+        #TODO: pipe this to logger-object instead of stdout
         print "Lost connection (%s), reconnecting." % (reason,)
         connector.connect()
 
     def clientConnectionFailed(self, connector, reason):
+        #TODO: pipe this to logger-object instead of stdout
         print "Could not connect: %s" % (reason,)
 
 
 
-###main loop
 
 import sys
 from twisted.internet import reactor
 
 if __name__ == "__main__":
         chan = sys.argv[1]
-        reactor.connectTCP('irc.freenode.net', 6667, TralalaBotFactory('#' + chan, "ChannelRoadie"))
+        reactor.connectTCP('irc.freenode.net', 6667, TralalaBotFactory('#' + chan))
         reactor.run()
